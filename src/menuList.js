b49b98d55d46877cf5391ec3e11ab3711341c44c
yuyue3 <yy18825237023@126.com>
2025-08-11 11:02:55 +0800

    Add files via upload

--- src/menuList.js
+++ src/menuList.js
@@ -1,6 +1,6 @@
 import Home from './pages/Home';
 import Users from './pages/Users';
-import About from './pages/About';
+import Database from './pages/Database';
 import HomeIcon from '@mui/icons-material/Home';
 import PeopleIcon from '@mui/icons-material/People';
 import InfoIcon from '@mui/icons-material/Info';
@@ -8,7 +8,7 @@ import InfoIcon from '@mui/icons-material/Info';
 const menuList = [
   { path: '/', label: '首页', icon: <HomeIcon />, element: <Home /> },
   { path: '/users', label: '用户管理', icon: <PeopleIcon />, element: <Users /> },
-  { path: '/about', label: '关于', icon: <InfoIcon />, element: <About /> }
+  { path: '/database', label: '数据管理', icon: <InfoIcon />, element: <Database /> }
 ];
 
 export default menuList;
